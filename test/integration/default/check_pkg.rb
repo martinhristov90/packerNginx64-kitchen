d1d7e02a3c2bb85b7f97aa9c7fd4f4c8aee269cc
Martin Hristov <martinhristov@Martins-MacBook-Pro.local>
2019-05-13 09:50:42 +0300

    Updated configuration

--- test/integration/default/check_pkg.rb
+++ test/integration/default/check_pkg.rb
@@ -1,3 +1,12 @@
 describe package('nginx') do
   it { should be_installed }
 end
+
+describe service('nginx') do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe http('http://localhost') do
+  its('status') { should cmp 200 }
+end
